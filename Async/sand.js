9282bbf4b5eb6b7fb9cb1641c6d8751626e940d9
Mahfooz <umahfoosahamed@gmail.com>
2021-05-16 10:59:10 +0530

    added http

--- Async/sand.js
+++ Async/sand.js
@@ -1,9 +1,23 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 
-setTimeout(() => {    // Asyncronous
-    console.log('callback function fired');
-}, 2000);  //2000 milli secconds
+// setTimeout(() => {    // Asyncronous
+//     console.log('callback function fired');
+// }, 2000);  //2000 milli secconds
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState);
+
+    if (request.readyState === 4) {
+        console.log(request.responseText);
+    }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
