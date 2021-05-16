7e96a064159bb02f679f6b2faf30aa04de942a44
Mahfooz <umahfoosahamed@gmail.com>
2021-05-16 11:18:20 +0530

    added callback

--- Async/sand.js
+++ Async/sand.js
@@ -8,19 +8,28 @@
 // console.log(3);
 // console.log(4);
 
+const getTodos = () => { 
 
-const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
+    request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
 
-    if (request.readyState === 4) {
+        if (request.readyState === 4 && request.status === 200) {
         console.log(request.responseText);
-    }
-});
+        } else if (request.readyState === 4 ){
+        console.log('could not fetch the data');
+        }
+    });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+
+};
+
+getTodos();
+
+
 
 // Status Codes 
 // 404  -  Wrong endpoint / not connected / cannot find 
