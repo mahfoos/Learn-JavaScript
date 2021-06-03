8ecc45cbe133d6a36f0791e2dfa7a7f913f82d17
Mahfooz <umahfoosahamed@gmail.com>
2021-06-03 19:22:16 +0530

    added json files

--- Async/sand.js
+++ Async/sand.js
@@ -1,3 +1,5 @@
+
+// Vedio 1
 // console.log(1);
 // console.log(2);
 
@@ -8,27 +10,31 @@
 // console.log(3);
 // console.log(4);
 
-const getTodos = () => { 
+// vedio 2
+// const request = new XMLHttpRequest();
 
-    const request = new XMLHttpRequest();
+// request.addEventListener('readystatechange', () => {
+//     console.log(request, request.readyState);
+// })
 
-    request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+// request.send();
 
-        if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
-        } else if (request.readyState === 4 ){
-        console.log('could not fetch the data');
-        }
-    });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.send();
+// vedio 3
+// const request = new XMLHttpRequest();
 
-};
+// request.addEventListener('readystatechange', () => {
+//     // console.log(request, request.readyState);
+//     if (request.readyState === 4 && request.status === 200) {
+//         console.log(request, request.responseText);
+//     } else if(request.readyState === 4 ){
+//         console.log('Could not fetch the data')
+//     }
+// });
 
-getTodos();
-
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+// request.send();
 
 
 // Status Codes 
@@ -36,3 +42,85 @@ getTodos();
 // 200  - everthing is correct
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+
+
+
+// Vedio 4
+// const getTodos = (callback) => { 
+
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', () => {
+//     // console.log(request, request.readyState);
+
+//         if (request.readyState === 4 && request.status === 200) {
+//         // console.log(request.responseText);
+//         callback(undefined, request.responseText);
+//         } else if (request.readyState === 4 ){
+//         // console.log('could not fetch the data');
+//         callback('Could not fetch the data', undefined);
+//         }
+//     });
+
+//     request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
+//     request.send();
+
+// };
+
+// console.log(1);
+// console.log(2);
+
+// getTodos((err, data) => {        // callback function
+//     console.log('callback fired');
+//     if (err){
+//         console.log("Could not fetch the data");
+//     }else{
+//         console.log(data);
+//     }
+// });  
+
+// console.log(3);
+// console.log(4);
+
+
+//  vedio 5
+
+const getTodos = (callback) => { 
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+   
+
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+        callback(undefined, data);
+
+        } else if (request.readyState === 4 ){
+        callback('Could not fetch the data', undefined);
+        }
+    });
+
+    request.open('GET', 'todos.json');
+    request.send();
+
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {        // callback function
+    console.log('callback fired');
+    if (err){
+        console.log("Could not fetch the data");
+    }else{
+        console.log(data); 
+    }
+});  
+
+console.log(3);
+console.log(4);
+
+
+
